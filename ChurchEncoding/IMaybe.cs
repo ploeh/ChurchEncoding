ed0b6390a5e8825336c18a798081e9eb79c25026
Mark Seemann <mark@ploeh.dk>
2018-05-04 08:35:06 +0200

    Add IMaybe interface
    In this commit, the interface has no implementations, so it serves no
    practical purpose. The motivation for the commit, though, is that you
    can check out this commit and do the exercise of implementing the
    interface.
    
    There ought to be two implementations of IMaybe<T>: Just<T> and
    Nothing<T>.

---
+++ ChurchEncoding/IMaybe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ploeh.Samples.ChurchEncoding
+{
+    public interface IMaybe<T>
+    {
+        TResult Match<TResult>(TResult nothing, Func<T, TResult> just);
+    }
+}
